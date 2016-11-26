840e9bfd763412537954548c2493e7857d530db0
simbi0nts <rustorka.matro@gmail.com>
2016-11-27 01:16:44 +0300

    make it better

--- TrackingApp/views.py
+++ TrackingApp/views.py
@@ -8,17 +8,20 @@ from .models import CurrentActiveMachines
 
 def index(request):
 
-    if request.method == 'POST':
-        form = BrandForm(request.POST)
-        if form.is_valid():
-            brand_name = form.cleaned_data['brand_name']
-            if brand_name.brand_id == 0:
-                query_results = CurrentActiveMachines.objects.all().order_by('machine_id')
-            else:
-                query_results = CurrentActiveMachines.objects.filter(brand_name=brand_name).order_by('machine_id')
-    else:
-        form = BrandForm()
-        query_results = CurrentActiveMachines.objects.all().order_by('machine_id')
+    try :
+        if request.method == 'POST':
+            form = BrandForm(request.POST)
+            if form.is_valid():
+                brand_name = form.cleaned_data['brand_name']
+                if brand_name.brand_id == 0:
+                    query_results = CurrentActiveMachines.objects.all().order_by('machine_id')
+                else:
+                    query_results = CurrentActiveMachines.objects.filter(brand_name=brand_name).order_by('machine_id')
+        else:
+            form = BrandForm()
+            query_results = CurrentActiveMachines.objects.all().order_by('machine_id')
+    except:
+        pass
     template = loader.get_template('TrackingApp/index.html')
     context = RequestContext(request, {
         'query_results': query_results,
