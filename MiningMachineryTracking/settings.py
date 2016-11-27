437be6c98fb8f3d905c0dda4d6990dd9f33ac019
simbi0nts <rustorka.matro@gmail.com>
2016-11-27 17:54:31 +0300

    2nd

--- MiningMachineryTracking/settings.py
+++ MiningMachineryTracking/settings.py
@@ -136,15 +136,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
+
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "/static/"),
     '/app/TrackingApp/static/',
-    '/home/MiningMachineryTracking/TrackingApp/static/',
-    'app/TrackingApp/static/',
-    'home/MiningMachineryTracking/TrackingApp/static/',
 ]
 
-STATIC_URL = "/MiningMachinaryTracking/static/"
-
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_URL = "/static/"
+#
+##PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+##STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+#
+#STATIC_ROOT = "/home/MiningMachinaryTracking/MiningMachinaryTracking/static/"
