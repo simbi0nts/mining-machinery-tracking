5058556cbdf3c5054742b169d3ec8cb330156f84
simbi0nts <rustorka.matro@gmail.com>
2016-11-27 18:11:00 +0300

    2nd

--- MiningMachineryTracking/settings.py
+++ MiningMachineryTracking/settings.py
@@ -137,13 +137,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 
+STATICFILES_DIRS = (
+    #os.path.normpath(os.path.join(BASE_DIR, '')),
+    #os.path.normpath(os.path.join(BASE_DIR, 'upload')),
+)
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "/static/"),
-    '/app/TrackingApp/static/',
-]
-
-STATIC_URL = "/static/"
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'static'))
 #
 ##PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 ##STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
