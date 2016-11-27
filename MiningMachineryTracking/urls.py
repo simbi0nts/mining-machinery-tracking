cb327b63214bed00a8552dbb44953dbd2fa26190
simbi0nts <rustorka.matro@gmail.com>
2016-11-27 18:50:32 +0300

    2nd

--- MiningMachineryTracking/urls.py
+++ MiningMachineryTracking/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^', include('TrackingApp.urls')),
     url(r'^admin/', admin.site.urls),
-]
-urlpatterns += staticfiles_urlpatterns()
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+#urlpatterns += staticfiles_urlpatterns()
