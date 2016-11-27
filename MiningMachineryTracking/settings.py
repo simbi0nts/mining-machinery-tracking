e9739a04fdd5c8b0afbc89a7366a9a0cf3153b10
simbi0nts <rustorka.matro@gmail.com>
2016-11-27 13:34:19 +0300

    make it better

--- MiningMachineryTracking/settings.py
+++ MiningMachineryTracking/settings.py
@@ -23,11 +23,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '2ymp*a79g@$jfgrnp$@d_#f=pb@xri+=u_ao5zv0vc)&0k!vqh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['mining-machinery-tracking.herokuapp.com',
+ALLOWED_HOSTS = [#'mining-machinery-tracking.herokuapp.com',
                  '127.0.0.1',
-                 '*']
+                 'localhost']
 
 
 # Application definition
@@ -134,7 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "/static"),
     '/var/www/static/',
 ]
 
